aa233c1f9982ab8a1aeb693bf264cdd44d756056
Linda <mac@macs-MacBook-Pro.local>
2021-09-17 12:40:43 -0400

    did arrayMaxConsecutiveSum

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,5 +1,17 @@
 export function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
+    let maxArr= 0;
+    let currentVal= 0;
+    let i= 0;
 
+    do {
+        currentVal= inputArray[i] + inputArray[i + 1];
+        if(currentVal > maxArr){
+            maxArr= currentVal;
+        }
+        i ++;
+    } while (i < inputArray.length - 1);
+
+    return maxArr;
 }
 
 // console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
