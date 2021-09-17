470270cc95dbb278a5e799bb35b11178222cb2d5
Linda <mac@macs-MacBook-Pro.local>
2021-09-16 22:04:32 -0400

    used recursion on arrayConversion

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,12 @@
 export function arrayChange(inputArray: number[]): number {
+    let numbersNeeded= 0;
 
+    for (let i= 0; i < inputArray.length; i++){
+        if (inputArray[i] < i + 1){
+            numbersNeeded += (i+1) - inputArray[i];
+        }
+    }
+    return numbersNeeded;
 }
 
 // console.log(arrayChange([1, 1, 1]));
--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -1,5 +1,40 @@
 export function arrayConversion(inputArray: number[]): number {
+    let i= 0;
+    let j= 0;
+    let oddNum= [];
+    let evenNum= [];
 
+    do {
+        oddNum.push(inputArray[i] + inputArray[i+1]);
+        i += 2;
+    } while (i < inputArray.length);
+
+    i= 0;
+
+    function repeatArr() {
+        if(i < oddNum.length){
+            evenNum.push(oddNum[i] * oddNum[i+1]);
+            i += 2;
+            repeatArr();
+        } 
+
+        if(j < evenNum.length){
+            oddNum= [];
+            oddNum.push(evenNum[j] + evenNum[j+1]);
+            j += 2;
+            repeatArr();
+        } 
+    }
+
+    repeatArr();
+
+    if (oddNum.length === 1) {
+        return +oddNum;
+    } 
+    
+    if (evenNum.length === 1) {
+        return +evenNum;
+    }
 }
 
 // console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
