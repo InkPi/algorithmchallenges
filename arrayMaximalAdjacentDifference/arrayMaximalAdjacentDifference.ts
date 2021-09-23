7668d966511f5863760978cb82425a735d0d7bee
Linda <mac@macs-MacBook-Pro.local>
2021-09-22 22:09:51 -0400

    did arrayMaximalAdjacentDifference

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,20 @@
 export function arrayMaximalAdjacentDifference(inputArray: number[]): number {
+    let maxDiff= 0;
+    let currentNum= 0;
 
+    function difference (x, y) {
+        return Math.abs(x - y);
+    }
+
+    for (let i= 0; i < inputArray.length; i++){
+        currentNum= difference(inputArray[i], inputArray[i+1]);
+
+        if(currentNum> maxDiff){
+            maxDiff= currentNum;
+        }
+    }
+
+    return maxDiff;
 }
 
 // console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
