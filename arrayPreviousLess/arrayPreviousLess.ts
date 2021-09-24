f2421e296a553a813d5cabebe3ae70c7a9d48cea
Linda <mac@macs-MacBook-Pro.local>
2021-09-23 20:09:16 -0400

    arrayPreviousLess

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,17 @@
 export function arrayPreviousLess(items: number[]): number[] {
+    let lessArr= [];
+    var lastNum = items.length - 1;
 
+    items.unshift(items[lastNum]);
+
+    for(let i=1; i < items.length; i++){
+        if (items[i] < items[i-1]){
+            lessArr.push(-1);
+        } else 
+        {lessArr.push(items[i-1]);}
+    }
+
+    return lessArr;
 }
 
 // console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
